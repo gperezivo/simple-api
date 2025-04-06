3c4411708b83fe44de9ee85cad7f5a41a6c9dd1f
gperezivo <guillermo.perez.ivo@gmail.com>
2025-04-06 10:51:58 +0200

    chore: upgrade to .NET 9.0 and update dependencies; remove obsolete files

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,18 @@
-FROM mcr.microsoft.com/dotnet/aspnet:6.0 AS base
+FROM mcr.microsoft.com/dotnet/aspnet:9.0 AS base
 WORKDIR /app
 EXPOSE 80
 EXPOSE 443
 
-FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
 COPY . /app
 WORKDIR /app
-RUN dotnet restore
-
 
 FROM build AS publish
-RUN dotnet publish -c Release -o /app/publish
+RUN dotnet publish simple-api.csproj -c Release -o /app/publish --self-contained true  --runtime linux-x64 
 
 FROM base AS final
 WORKDIR /app
 COPY --from=publish /app/publish .
 ENV DATABASE__NAME="DockerDatabase"
 ENV APIURL="https://pokeapi.co/api/v2/"
-ENTRYPOINT dotnet simpleApi.dll
+ENTRYPOINT ["./simple-api"]
--- Program.cs
+++ Program.cs
@@ -2,94 +2,108 @@ using Microsoft.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-app.MapGet("/", (HttpContext ctx, IConfiguration config)  => {
-        var myVar = config.GetSection("MyVar")?.Value;
-        var version = config.GetSection("Version")?.Value;
-        
-        return new { 
-            MyVar= myVar ?? "Hello World" , 
-            MyVar2= config.GetValue<string>("MyVar2") ?? "Hello World",
-            version = version
-        };
-    }
+app.MapGet("/", (HttpContext ctx, IConfiguration config) =>
+{
+    var myVar = config.GetSection("MyVar")?.Value;
+    var version = config.GetSection("Version")?.Value;
+
+    return new
+    {
+        MyVar = myVar ?? "Hello World",
+        MyVar2 = config.GetValue<string>("MyVar2") ?? "Hello World",
+        version = version
+    };
+}
 );
-app.MapGet("/external-api", (HttpContext ctx, IConfiguration config)  => {
-        var apiUrl = config.GetSection("ApiUrl")?.Value;
-        var version = config.GetSection("Version")?.Value;
-        //call to GET apiUrl 
-        var httpClient = new HttpClient();
-        var response = httpClient.GetAsync(apiUrl).Result;
-        var content = response.Content.ReadAsStringAsync().Result;
-        return new {
-            ApiResponse= content,
-            version= version
-        };
-    }
+app.MapGet("/external-api", async (HttpContext ctx, IConfiguration config, IHttpClientFactory httpClientFactory) =>
+{
+    var apiUrl = config.GetSection("ApiUrl")?.Value;
+    var version = config.GetSection("Version")?.Value;
+
+    var httpClient = httpClientFactory.CreateClient();
+    var response = await httpClient.GetAsync(apiUrl);
+    var content = await response.Content.ReadAsStringAsync();
+
+    return new
+    {
+        ApiResponse = content,
+        version = version
+    };
+}
 );
-app.MapGet("/sql-connection", (HttpContext ctx, IConfiguration config)  => {
-       var sqlConnectionBuilder = new SqlConnectionStringBuilder(){
-            DataSource=config.GetSection("Database").GetValue<string>("Server"),
-            UserID=config.GetSection("Database").GetValue<string>("User"),
-            Password=config.GetSection("Database").GetValue<string>("Password"),
-            InitialCatalog=config.GetSection("Database").GetValue<string>("Name"),
-            MultipleActiveResultSets=true,
-            ConnectTimeout=30
-        };
-        var version = config.GetSection("Version")?.Value;
-        return new {
-            SqlConnection = sqlConnectionBuilder.ConnectionString,
-            version= version
-        };
-    }
+app.MapGet("/sql-connection", (HttpContext ctx, IConfiguration config) =>
+{
+    var sqlConnectionBuilder = new SqlConnectionStringBuilder()
+    {
+        DataSource = config.GetSection("Database").GetValue<string>("Server"),
+        UserID = config.GetSection("Database").GetValue<string>("User"),
+        Password = config.GetSection("Database").GetValue<string>("Password"),
+        InitialCatalog = config.GetSection("Database").GetValue<string>("Name"),
+        MultipleActiveResultSets = true,
+        ConnectTimeout = 30
+    };
+    var version = config.GetSection("Version")?.Value;
+    return new
+    {
+        SqlConnection = sqlConnectionBuilder.ConnectionString,
+        version = version
+    };
+}
 );
-app.MapGet("/sql", async (HttpContext ctx, IConfiguration config)  => {
-        
-        var sqlConnectionBuilder = new SqlConnectionStringBuilder(){
-            DataSource=config.GetSection("Database").GetValue<string>("Server"),
-            UserID=config.GetSection("Database").GetValue<string>("User"),
-            Password=config.GetSection("Database").GetValue<string>("Password"),
-            InitialCatalog=config.GetSection("Database").GetValue<string>("Name"),
-            MultipleActiveResultSets=true,
-            ConnectTimeout=30
-        };
-        var version = config.GetSection("Version")?.Value;
-        var connection = new SqlConnection(sqlConnectionBuilder.ConnectionString);
-        await connection.OpenAsync();
-        var command = new SqlCommand("SELECT * FROM sys.tables", connection);
-        var reader = await command.ExecuteReaderAsync();
-        var result = new List<dynamic>();
-        while (await reader.ReadAsync())
+app.MapGet("/sql", async (HttpContext ctx, IConfiguration config) =>
+{
+
+    var sqlConnectionBuilder = new SqlConnectionStringBuilder()
+    {
+        DataSource = config.GetSection("Database").GetValue<string>("Server"),
+        UserID = config.GetSection("Database").GetValue<string>("User"),
+        Password = config.GetSection("Database").GetValue<string>("Password"),
+        InitialCatalog = config.GetSection("Database").GetValue<string>("Name"),
+        MultipleActiveResultSets = true,
+        ConnectTimeout = 30
+    };
+    var version = config.GetSection("Version")?.Value;
+    using var connection = new SqlConnection(sqlConnectionBuilder.ConnectionString);
+    await connection.OpenAsync();
+    using var command = new SqlCommand("SELECT * FROM sys.tables", connection);
+    var reader = await command.ExecuteReaderAsync();
+    var result = new List<dynamic>();
+    while (await reader.ReadAsync())
+    {
+        result.Add(new
         {
-            result.Add(new {
-                Id= reader.GetInt32(0),
-                Name= reader.GetString(1)
-            });
-        }
-        return new {
-            SqlResponse= result,
-            version=version
-        };
+            Id = reader.GetInt32(0),
+            Name = reader.GetString(1)
+        });
     }
+    return new
+    {
+        SqlResponse = result,
+        version = version
+    };
+}
 );
 app.MapGet("/ip",
-    (HttpContext ctx, IConfiguration config)  => {
-        var ip = ctx.Connection.RemoteIpAddress.ToString();
-        
-        return new {
-            Ip= ip
+    (HttpContext ctx, IConfiguration config) =>
+    {
+        var ip = ctx.Connection.RemoteIpAddress?.ToString() ?? string.Empty;
+
+        return new
+        {
+            Ip = ip
         };
     }
 );
 app.MapGet("/sort",
-    (HttpContext ctx, IConfiguration config, int count) => {
-        // Create an array of 1000 randoms ints
-        if(count==0) count=1000;
+    (HttpContext ctx, IConfiguration config, int count) =>
+    {
+        if (count == 0) count = 1000;
         var randoms = new int[count];
+        var random = new Random();
         for (int i = 0; i < randoms.Length; i++)
         {
-            randoms[i] = new Random().Next(0, 1000);
+            randoms[i] = random.Next(0, 1000);
         }
-        // Sort the array with bubble sort
         for (int i = 0; i < randoms.Length; i++)
         {
             for (int j = 0; j < randoms.Length - 1; j++)
@@ -105,4 +119,4 @@ app.MapGet("/sort",
         return Task.FromResult(new { value = randoms });
     });
 
-app.Run();
+await app.RunAsync();
